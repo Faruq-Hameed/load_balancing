b9a8e14316ca9cfebabe3937cba5975fc6603990
Faruq-Hameed <faruqhameed1@gmail.com>
2023-07-03 09:35:52 +0100

    used express web serverdepencies

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 
 - A load balancer acts as the traffic cop sitting in front of your application servers and routing client requests across all servers capable of fulfilling those requests in a manner that maximizes speed and capacity utilization and ensures that no one server is overworked, which could degrade performance.
 
-NodeJS has a built-in module called <b>Cluster Module </b> to take the advantage of a multi-core system. Using this module you can launch NodeJS instances to each core of your system
-
 -To see the performance, we have used <b> autocannon tools</b> to test our server. The Autocannon package has already been installed globally on the machine.
+
+### List of tested load balancers;
+- Cluster modules
+- Using express web server
+
+- Nginx can also be install on the machine
---
+++ express_l_balncer.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app1 = express();
+const app2 = express();
+
+const logger = require('morgan');
+
+//logger
+app1.use(logger('dev'))
+app2.use(logger('dev'))
+
+
+// Handler method
+const handler = num => (req,res)=>{
+	const { method, url, headers, body } = req;
+	res.send('Response from server ' + num);
+}
+
+// Only handle GET and POST requests
+// Receive request and pass to handler method
+app1.get('*', handler(1)).post('*', handler(1));
+app2.get('*', handler(2)).post('*', handler(2));
+
+// Start server on PORT 3000
+app1.listen(3000, err =>{
+	err ?
+	console.log("Failed to listen on PORT 3000"):
+	console.log("Application Server listening on PORT 3000");
+});
+
+// Start server on PORT 3001
+app2.listen(3001, err =>{
+	err ?
+	console.log("Failed to listen on PORT 3001"):
+	console.log("Application Server listening on PORT 3001");
+});
